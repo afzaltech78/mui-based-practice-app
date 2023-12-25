3c431b26ed5c5fc29b33fa90125bf25af8c2179b
afzal_gsoft <afzal@gsoftconsulting.com>
2023-12-25 23:03:29 +0500

    feat:update things

---
+++ app/(pages)/register/page.tsx
@@ -0,0 +1,16 @@
+/** @format */
+
+import { AuthHeader } from "@/app/components/AuthHeader";
+import { LoginForm } from "@/app/components/LoginForm";
+import styles from "../../assets/css/pages/Login.module.css";
+
+const Register = () => {
+  return (
+    <div className={styles.container}>
+      <AuthHeader />
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Register;
