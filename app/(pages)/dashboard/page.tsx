c159ecd6163ac9bd853d3ac7c6a829ceebdd87d6
afzal_gsoft <afzal@gsoftconsulting.com>
2024-04-05 15:40:26 +0500

    feat:updated

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,12 +1,53 @@
 /** @format */
 
 import { Header } from "@/app/components/Header";
-import styles from "../../assets/css/pages/Login.module.css";
+import styles from "../../assets/css/pages/Dashboard.module.css";
+import { Grid, Typography } from "@mui/material";
+import { ProductCard } from "@/app/components/ProductCard";
 
 const dashboard = () => {
   return (
     <div className={styles.container}>
       <Header />
+      <div className={styles.container2}>
+        {/* all content comes bellow */}
+        <Typography
+          variant="h6"
+          component="div"
+          color={"#8C8888"}
+          fontSize={"16px"}
+          marginTop={"14px"}>
+          Welcome
+        </Typography>
+        <Typography
+          variant="h1"
+          component="div"
+          fontSize={"26px"}
+          fontWeight={"600"}>
+          Select Venue
+        </Typography>
+
+        {/* product cards */}
+        {/* <div className={styles.productsContainer}>
+          {[0, 1, 2, 3, 4, 5, 6].map(() => (
+            <ProductCard />
+          ))}
+        </div> */}
+        <Grid
+          display={"flex"}
+          // flexWrap={"wrap"}
+          flexDirection={"row"}
+          gap={"25px"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+          container>
+          {[0, 1, 2, 3, 4, 5, 6].map(() => (
+            <Grid sm={12} lg={3} item>
+              <ProductCard />
+            </Grid>
+          ))}
+        </Grid>
+      </div>
     </div>
   );
 };
---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,8 @@
+/** @format */
+
+import { Button, ButtonProps } from "@mui/material";
+import styles from "../assets/css/components/ProductCard.module.css";
+
+export const ProductCard = (props) => {
+  return <div className={`${styles.container}`}></div>;
+};
